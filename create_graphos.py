83c7319312c09e6b06bde118f75842b290eca91b
anaaliceferza1 <analiceferza2025@gmail.com>
2026-03-25 18:58:11 -0300

    adicionando altitudes e seus pesos referente

--- create_graphos.py
+++ create_graphos.py
@@ -4,35 +4,53 @@ matplotlib.use('TkAgg')  # ou 'Qt5Agg'
 
 import matplotlib.pyplot as plt
 
-class Diagraphs:
+class Dgraphs:
     def __init__(self):
         self.graph = nx.DiGraph()
 
     def create_graphs(self):
-        nodes = input("Digite os nós separados por vírgula: ").split(",")
+        nodes = input("Digite os vértices(n) separados por vírgula: ").split(",")
         self.graph.add_nodes_from([n.strip() for n in nodes])
 
-        m = int(input("Quantas arestas? "))
+        for node in self.graph.nodes():
+            altitude = int(input(f"Digite a altitude do vértice(n) {node}: "))
+            self.graph.nodes()[node]['altitude'] = altitude
+
+        m = int(input("Quantas arestas(m)? "))
         for _ in range(m):
             origin, destine = input("Origem->Destino: ").split("->")
             self.graph.add_edge(origin.strip(), destine.strip())
 
     def weight_graph(self):
-        for edge in self.graph.edges():
-            weight = int(input(f"Peso da aresta {edge}: "))
-            self.graph[edge[0]][edge[1]]['weight'] = weight
-    
-    def show_graphs_png(self):
-        plt.figure()
-        pos = nx.spring_layout(self.graph)
-        nx.draw(self.graph, pos, with_labels=True, node_color="lightblue", edge_color="gray")
-        labels = nx.get_edge_attributes(self.graph, 'weight')
-        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=labels)
+        for v in self.graph.nodes():
+            for u in self.graph.successors(v):
+                if v != u:
+                    edge = (v, u)
+                    diff = self.graph.nodes()[u]['altitude'] - self.graph.nodes()[v]['altitude']
+                    if diff > 0:
+                        w = diff*2
+                        self.graph.edges[edge]['weight'] = w
+                    else:
+                        w = diff/2
+                        self.graph.edges[edge]['weight'] = w
+
+    def draw_graphs(self):
+        pos = nx.spring_layout(self.graph, seed=42)
+        
+        labels = {node:f"{node}\n{self.graph.nodes()[node]['altitude']}m" for node in self.graph.nodes()}
+        
+        edge_labels = {(u, v):f"{d['weight']:.1f}" for u, v, d in self.graph.edges(data=True)}
+        
+        node_colors = ["orange" if self.graph.nodes()[node]['altitude'] == max(nx.get_node_attributes(self.graph, 'altitude').values()) else "lightblue" for node in self.graph.nodes()]
+        
+        nx.draw(self.graph, pos, with_labels=False, node_color=node_colors, node_size=1500, arrowsize=True)
+        nx.draw_networkx_labels(self.graph, pos, labels=labels)
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
+        plt.title("Ilha e seus locais")
         plt.show()
 
     def show_graph(self):
         print("\nGrafo:")
         for no in self.graph.nodes():
-            exit_degree = list(self.graph.successors(no)) 
-            entry_degree = list(self.graph.predecessors(no))
-            print(f"{no} -> Sair: {exit_degree}, Entrar: {entry_degree}")
+            neigh = list(self.graph.successors(no)) 
+            print(f"{no} -> Vizinhos: {neigh}")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from create_graphos import Diagraphs
+from create_graphos import Dgraphs
 import networkx as nx3
 import matplotlib
 matplotlib.use('TkAgg')
 
 import matplotlib.pyplot as plt
 
-g = Diagraphs()
+g = Dgraphs()
 g.create_graphs()
 g.show_graphs_png()
 g.show_graph()
