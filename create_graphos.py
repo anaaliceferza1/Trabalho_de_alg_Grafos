7d89570dd688788c0384c5c25267504f7c99b8e7
Rebeca Sindeaux <sindeauxrebeca@gmail.com>
2026-03-25 19:02:58 -0300

    Função Check_Dag verifica se nao eh DAG. Apos detectar deleta o erro

--- create_graphos.py
+++ create_graphos.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import matplotlib
+import numpy as np
 matplotlib.use('TkAgg')  # ou 'Qt5Agg'
 
 import matplotlib.pyplot as plt
+#pip3 install networkx matplotlib numpy
 
 class Diagraphs:
     def __init__(self):
@@ -21,6 +23,52 @@ class Diagraphs:
         for edge in self.graph.edges():
             weight = int(input(f"Peso da aresta {edge}: "))
             self.graph[edge[0]][edge[1]]['weight'] = weight
+        self.check_dag()
+
+    def check_dag(self):
+        while True:
+            nodes = list(self.graph.nodes())
+            n = len(nodes)
+            if n == 0: break
+
+            adj = nx.to_dict_of_dicts(self.graph)
+            c = np.full((n + 1, n + 1), float('inf'))
+            c[0, 0] = 0
+
+            idx_to_node = {i+1: node for i, node in enumerate(nodes)}
+            node_to_idx = {node: i+1 for i, node in enumerate(nodes)}
+            for l in range(1, n + 1):
+                for k in range(n + 1):
+                    val_min = c[l-1, k]
+                    
+                    for i in range(n + 1):
+                        weight = None
+                        if i == 0 and k > 0: 
+                            weight = 0
+                        elif i > 0 and k > 0:
+                            u_name, v_name = idx_to_node[i], idx_to_node[k]
+                            if self.graph.has_edge(u_name, v_name):
+                                weight = self.graph[u_name][v_name].get('weight', 0)
+                        
+                        if weight is not None:
+                            val_min = min(val_min, c[l-1, i] + weight)
+                    
+                    c[l, k] = val_min
+                    
+            cycle_node_idx = None
+            for k in range(1, n + 1):
+                if c[n, k] != c[n-1, k]:
+                    cycle_node_idx = k
+                    break
+            
+            if cycle_node_idx:
+                node_name = idx_to_node[cycle_node_idx]
+                edge_to_remove = list(self.graph.in_edges(node_name))[0]
+                print(f"->Ciclo detectado:'{node_name}'. Removendo {edge_to_remove}")
+                self.graph.remove_edge(*edge_to_remove)
+            else:
+                print("O grafo é um DAG.")
+                break
     
     def show_graphs_png(self):
         plt.figure()
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from create_graphos import Diagraphs
 import networkx as nx3
 import matplotlib
+import numpy as np
 matplotlib.use('TkAgg')
 
 import matplotlib.pyplot as plt
 
 g = Diagraphs()
 g.create_graphs()
+g.weight_graph()
 g.show_graphs_png()
 g.show_graph()
-
 
 
